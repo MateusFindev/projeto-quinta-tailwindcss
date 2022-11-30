f266665e6eb27f385c5da0fc92b94221dfbba5c7
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-11-30 18:40:25 -0300

    a

--- routes/salas.js
+++ routes/salas.js
@@ -5,12 +5,36 @@ var router = express.Router();
 var database = require('../database');
 
 router.get('/', function (req, res, next) {
-  database.query('SELECT * FROM sala ORDER BY id desc', function (err, rows) {
+  const info = []
+  database.query('SELECT * FROM sala ORDER BY id ASC', function (err, rows) {
     if (err) {
       req.flash('error', err)
       res.render('salas', { data: '' })
     } else {
-      res.render('salas', { data: rows })
+      database.query('SELECT * FROM professor_sala ORDER BY id ASC', function (err2, rows2) {
+        if (err) {
+          req.flash('error', err2)
+          res.render('salas', { data: '' })
+        } else {
+          let professor = "";
+          let sala = "";
+          let professores = [];
+
+          for (let i = 0; i < rows2.length; i++) {
+            sala = rows2[i].ID_sala;
+            database.query(`SELECT * FROM professor WHERE id=${rows2[i].ID_professor}`, function (err3, rows3) {
+              professores.push(rows3)
+              
+              if (i == rows2.length - 1) {
+                info.push(rows)
+                info.push(rows2)
+                info.push(professores)
+                res.render('salas', { data: info });
+              }
+            })
+          }
+        }
+      })
     }
   })
 })
