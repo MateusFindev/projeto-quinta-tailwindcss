1c91f20e951eeb88d4c4b69eb7eda6b14a2438fc
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-11-30 22:11:00 -0300

    routes for deleting classroom, register and login

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const configuracoesRouter = require('./routes/configuracoes');
 const ajudaRouter = require('./routes/ajuda');
 const perfilRouter = require('./routes/perfil');
 const loginRouter = require('./routes/login');
-const addSala = require('./routes/adicionar_sala');
+const cadastroRouter = require('./routes/cadastro');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -44,7 +44,7 @@ app.use('/configuracoes', configuracoesRouter);
 app.use('/ajuda', ajudaRouter);
 app.use('/perfil', perfilRouter);
 app.use('/login', loginRouter);
-app.use('/adicionar_sala', addSala);
+app.use('/cadastro', cadastroRouter);
 
 app.post('/save', function (req, res, next) {
   var name = req.body.name;
@@ -61,16 +61,96 @@ app.post('/save', function (req, res, next) {
   })
 })
 
-app.post('/adicionar', function (req, res, next) {
+app.post('/cadastrar', function (req, res, next) {
   var name = req.body.name;
-  var password = req.body.password;
+  var lastname = req.body.lastname;
+  var email = req.body.email;
+  var password = req.body.passconfirmation;
 
-  var sql = `INSERT INTO sala (nome, senha) VALUES ("${name}", "${password}")`;
+  var sql = `INSERT INTO alunos (nome, senha, email) VALUES ("${name + " " + lastname}", "${password}", "${email}")`;
 
   db.query(sql, function (err, result) {
     if (err) throw err
     console.log('Row has been updated')
-    res.redirect('/salas')
+    // req.flash('success', 'Data stored!')
+    res.redirect('http://localhost:3000/')
+  })
+})
+
+app.post('/entrar', function (req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  var sql = "SELECT * FROM alunos ORDER BY id DESC";
+
+  db.query(sql, function (err, result) {
+    if (err) {
+      throw err
+    }
+    if (result) {
+      for (var i=0; i < result.length; i++) {
+        if (email == result[i].email) {
+          if (password == result[i].senha) {
+            console.log('Entrou');
+            res.redirect('/inicio')
+          } else {
+            console.log("Senha incorreta");
+          }
+        } else {
+          console.log("Email incorreto")
+        }
+      }
+    }
+  })
+})
+
+app.post('/excluir', function(req, res, next) {
+  var sala = req.body.salaID;
+
+  var sql = `DELETE FROM professor_sala WHERE ID_sala=${sala}`;
+  var sql2 = `DELETE FROM sala WHERE id=${sala}`;
+
+  db.query(sql, function (err, result) {
+    if (err) {
+      throw err
+    }
+    db.query(sql2, function (err, result) {
+      if (err) {
+        throw err
+      }
+      console.log('Row has been updated')
+      res.redirect('/salas')
+    })
+  })
+});
+
+app.post('/adicionar', function (req, res, next) {
+  var name = req.body.name;
+  var password = req.body.password;
+  var professor = req.body.professor;
+
+  var sql = `INSERT INTO sala (nome, senha) VALUES ("${name}", "${password}")`;
+
+  db.query('SELECT ID FROM sala ORDER BY id ASC', function (err, rows) {
+    if (err) {
+      req.flash('error', err)
+      res.render('salas', { data: '' })
+    } else {
+      var sql2 = `INSERT INTO professor_sala (ID_professor, ID_sala) VALUES ("${professor}", "${rows[0].ID}")`;
+
+      db.query(sql, function (err, result) {
+        if (err) {
+          throw err
+        }
+        db.query(sql2, function (err, result) {
+          if (err) {
+            throw err
+          }
+          console.log('Row has been updated')
+          res.redirect('/salas')
+        })
+      })
+    }
   })
 })
 
