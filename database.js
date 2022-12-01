16820d22f0d1b10e0eb4f8727d0aaf390c7d1880
MateusFindev <85619574+MateusFindev@users.noreply.github.com>
2022-12-01 00:56:13 -0300

    Updated "perfil" and modal created

--- database.js
+++ database.js
@@ -1,21 +1,21 @@
-const mysql = require('mysql');
-const dotenv = require('dotenv').config();
+// const mysql = require('mysql');
+// const dotenv = require('dotenv').config();
 
-// bksmlosyfgid6iwcckvm-mysql.services.clever-cloud.com
+// // bksmlosyfgid6iwcckvm-mysql.services.clever-cloud.com
 
-let connection = mysql.createConnection({
-	host : process.env.HOST,
-	database : process.env.DB,
-	user : process.env.USER,
-	password : process.env.PW
-});
+// let connection = mysql.createConnection({
+// 	host : process.env.HOST,
+// 	database : process.env.DB,
+// 	user : process.env.USER,
+// 	password : process.env.PW
+// });
 
-connection.connect(function(error){
-	if(error) {
-		throw error;
-	} else {
-		console.log('MySQL Database is connected Successfully');
-	}
-});
+// connection.connect(function(error){
+// 	if(error) {
+// 		throw error;
+// 	} else {
+// 		console.log('MySQL Database is connected Successfully');
+// 	}
+// });
 
-module.exports = connection;
+// module.exports = connection;
